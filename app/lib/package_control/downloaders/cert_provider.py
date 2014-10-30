81c07318b149b3a82e45aa1c0392733e762c461a
wbond <will@wbond.net>
2014-10-30 12:33:53 -0400

    Fixed the cert_provider to work on my OS X development machine

--- app/lib/package_control/downloaders/cert_provider.py
+++ app/lib/package_control/downloaders/cert_provider.py
@@ -1,4 +1,5 @@
 # Not shared with Package Control
+from ..ca_certs import get_system_ca_bundle_path
 
 
 class CertProvider(object):
@@ -20,5 +21,10 @@ class CertProvider(object):
         :return:
             The CA cert bundle path on success, or False on error
         """
+
+        # Try to use the system CA bundle
+        ca_bundle_path = get_system_ca_bundle_path(self.settings)
+        if ca_bundle_path:
+            return ca_bundle_path
 
         return '/etc/ssl/certs/ca-certificates.crt'
