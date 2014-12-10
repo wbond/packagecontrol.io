3041725c71e8718bc48b3664d96da45ed783864d
wbond <will@wbond.net>
2014-12-10 09:05:27 -0500

    Don't compute the bz2 package hash anymore since we use dependency functionality to obtain that now

--- app/tasks/compute_package_hashes.py
+++ app/tasks/compute_package_hashes.py
@@ -5,7 +5,7 @@ import os
 root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 assets = os.path.join(root, 'assets')
 
-for package in ['Package Control.sublime-package', 'bz2.sublime-package', 'ssl-linux.sublime-package', 'ssl-windows.sublime-package']:
+for package in ['Package Control.sublime-package', 'ssl-linux.sublime-package', 'ssl-windows.sublime-package']:
     file_path = os.path.join(assets, package)
     with open(file_path, 'rb') as f:
         hash_ = hashlib.sha256(f.read()).hexdigest()
