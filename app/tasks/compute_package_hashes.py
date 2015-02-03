ee19a6ab43dec95cfa982e86ac91f5e53eb9c636
wbond <will@wbond.net>
2015-02-03 12:17:09 -0500

    Updated package computation to handle new assets/ssl/ dir

--- app/tasks/compute_package_hashes.py
+++ app/tasks/compute_package_hashes.py
@@ -4,9 +4,17 @@ import os
 
 root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 assets = os.path.join(root, 'assets')
+ssl_dir = os.path.join(assets, 'ssl')
 
-for package in ['Package Control.sublime-package', 'ssl-linux.sublime-package', 'ssl-windows.sublime-package']:
-    file_path = os.path.join(assets, package)
-    with open(file_path, 'rb') as f:
+
+pc_path = os.path.join(assets, 'Package Control.sublime-package')
+paths = [pc_path]
+for (root, dirs, files) in os.walk(ssl_dir):
+    for file in files:
+        paths.append(os.path.join(ssl_dir, root, file))
+
+for path in paths:
+    with open(path, 'rb') as f:
         hash_ = hashlib.sha256(f.read()).hexdigest()
-        print('%s: %s' % (package, hash_))
+        filename = path.replace(assets + '/', '')
+        print('%s: %s' % (filename, hash_))
