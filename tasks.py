788c97c73dcf21e2fd0e6326c7924b12bc845e18
wbond <will@wbond.net>
2015-06-08 20:04:59 -0400

    Updated tasks to write rotating log files

--- tasks.py
+++ tasks.py
@@ -11,6 +11,8 @@ from app.lib import processes
 root_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(root_dir)
 
+logs_root = os.path.join(root_dir, 'task_logs')
+
 tasks_root = os.path.join(root_dir, 'app/tasks')
 valid_tasks = []
 for filename in os.listdir(tasks_root):
@@ -36,6 +38,15 @@ for pid, command_line in processes.list_all():
     if match.group(2) == args.task_name:
         print('Another instance of the %s task is currently running' % args.task_name)
         sys.exit(0)
+
+for num in range(10, 0, -1):
+    source_file = os.path.join(logs_root, '%s.%d.log' % (args.task_name, num - 1))
+    dest_file = os.path.join(logs_root, '%s.%d.log' % (args.task_name, num))
+    if os.path.exists(source_file):
+        os.rename(source_file, dest_file)
+
+log_file = os.path.join(logs_root, '%s.0.log' % args.task_name)
+sys.stdout = open(log_file, 'w', encoding='utf-8')
 
 # Put extra params in sys.argv so the tasks can get them
 sys.argv = [args.task_name + '.py']
