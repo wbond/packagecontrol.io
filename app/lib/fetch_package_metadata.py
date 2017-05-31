2cab373171a7323c265658dcb048c9d5cb3c1d09
wbond <will@wbond.net>
2017-05-31 16:15:49 -0400

    Remove repository URL from messages when using /fetch.json

--- app/lib/fetch_package_metadata.py
+++ app/lib/fetch_package_metadata.py
@@ -25,12 +25,12 @@ def fetch_package_metadata(spec):
             config.read_secret('github_client_secret')
     settings['debug'] = False
 
-    def clean_url(exception):
+    def clean_message(exception):
         error = exception.args[0]
         for param, value in settings['query_string_params']['api.github.com'].items():
             regex = '[?&]' + re.escape(param) + '=' + re.escape(value)
             error = re.sub(regex, '', error)
-        return error
+        return error.replace('in the repository https://example.com', 'in the repository')
 
     provider = RepositoryProvider('https://example.com', settings)
     provider.schema_version = '3.0.0'
@@ -43,14 +43,14 @@ def fetch_package_metadata(spec):
 
         if provider.failed_sources:
             source, e = provider.failed_sources.popitem()
-            return {'result': 'error', 'message': clean_url(e)}
+            return {'result': 'error', 'message': clean_message(e)}
 
         if provider.broken_packages:
             name, e = provider.broken_packages.popitem()
-            return {'result': 'error', 'message': '%s: %s' % (name, clean_url(e))}
+            return {'result': 'error', 'message': '%s: %s' % (name, clean_message(e))}
 
     except (Exception) as e:
-        return {'result': 'error', 'message': clean_url(e)}
+        return {'result': 'error', 'message': clean_message(e)}
 
     finally:
         close_all_connections()
