cd26cfbf14f8d9a43595bf3195f338353d7d753f
wbond <will@wbond.net>
2020-09-10 22:37:08 -0400

    Update to latest package_control lib

--- app/lib/package_control/sys_path.py
+++ app/lib/package_control/sys_path.py
@@ -2,4 +2,10 @@
 
 import os
 
-pc_cache_dir = os.path.join(os.path.expanduser('~'), '.package_control')
+
+def pc_cache_dir():
+    return os.path.join(os.path.expanduser('~'), '.package_control')
+
+
+def user_config_dir():
+    return pc_cache_dir()
