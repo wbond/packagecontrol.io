79836437ca80846e5a2a1a45c1dec0cca6a3fa73
deathaxe <deathaxe82@googlemail.com>
2023-10-29 09:39:18 +0100

    Adjustable cache path

--- app/lib/package_control/sys_path.py
+++ app/lib/package_control/sys_path.py
@@ -2,9 +2,16 @@
 
 import os
 
+__cache_path = os.path.join(os.path.expanduser('~'), '.package_control')
+
+
+def set_cache_dir(cache_path):
+    global __cache_path
+    __cache_path = cache_path
+
 
 def pc_cache_dir():
-    return os.path.join(os.path.expanduser('~'), '.package_control')
+    return __cache_path
 
 
 def user_config_dir():
